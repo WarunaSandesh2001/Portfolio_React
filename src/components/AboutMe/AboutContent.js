721e5fccacdf473de7b21242e12e1e73d372d92d
WarunaSandesh2001 <warunasandesh2001@gmail.com>
2022-10-22 13:11:46 +0530

    Added About Me second component

--- src/components/AboutMe/AboutContent.js
+++ src/components/AboutMe/AboutContent.js
@@ -1,15 +1,35 @@
 import './AboutContentStyles.css'
 
+import { Link } from 'react-router-dom'
+
+import Language1 from "../../assets/carrental.jpg"
+import Language2 from "../../assets/background2.jpg"
+
 import React from 'react'
 
 const AboutContent = () => {
   return (
     <div className='about-container'>
-        <div className='left'>
-            <h1>Who Am I?</h1>
-            <p>I'm a FullStack developer. I create responsive secure websites for my clients.</p>
-            <button to="" className="btnView">View More</button>
+      <div className='left'>
+        <h1>Who Am I?</h1>
+        <p>I'm a FullStack developer. I create responsive secure websites for my clients.</p>
+        <Link to="/contact">
+          <button to="" className="btn" style={{ width: "30%" }}>Contact</button>
+        </Link>
+      </div>
+      <div className='right'>
+        <div className='img-container'>
+          <div className='img-stack top'>
+            <img src={Language1} className="img" alt='true' />
+          </div>
+          <div className='img-stack bottom'>
+            <img src={Language2} className="img" alt='true' />
+          </div>
+          <div className='img-stack right'>
+            <img src={Language1} className="img" alt='true' />
+          </div>
         </div>
+      </div>
     </div>
   )
 }
